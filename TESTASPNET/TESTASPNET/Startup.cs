6daaad6afd2de6807d2cdcf43277c3240021c3c4
Rafiq Ahmed <rafiq1983@gmail.com>
2017-04-07 16:34:31 -0400

    Testing

---
+++ TESTASPNET/TESTASPNET/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TESTASPNET.Startup))]
+namespace TESTASPNET
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
